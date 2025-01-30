715b6166bcf6a3bb242565d1161c314c0d145740
MehediAlam49 <mehedialam806@gmail.com>
2025-01-30 12:06:15 +0600

    print the array using fixed index in index array

---
+++ arrays.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h1>Arrays</h1>";
+
+$fruits = array("apple", "banana", "cherry");
+// print the array using fixed index in index array
+echo $fruits[0]."<br>";
+echo $fruits[1]."<br>";
+echo $fruits[2]."<br>";
+
+?>
