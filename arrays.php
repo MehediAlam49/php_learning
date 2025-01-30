597cb2fb142eaf106ef5c7f270b162adbd925c57
MehediAlam49 <mehedialam806@gmail.com>
2025-01-30 12:20:44 +0600

    print the array using for loop in index array

--- arrays.php
+++ arrays.php
@@ -1,8 +1,9 @@
 <?php
-echo "<h1>Arrays</h1>";
+echo "<h2>Index Array</h2>";
 
 $fruits = array("apple", "banana", "cherry", "orange", "kiwi");
 // print the array using fixed index in index array
+echo "print the array using fixed index in index array <br>";
 echo $fruits[0]."<br>";
 echo $fruits[1]."<br>";
 echo $fruits[2]."<br>";
@@ -10,6 +11,7 @@ echo $fruits[3]."<br>";
 echo $fruits[4]."<br>";
 
 // print the array using unordered list in index array
+echo "<br> print the array using unordered list in index array <br>";
 echo "<ul>";
 echo "<li>".$fruits[0]."</li>";
 echo "<li>".$fruits[1]."</li>";
@@ -17,4 +19,15 @@ echo "<li>".$fruits[2]."</li>";
 echo "<li>".$fruits[3]."</li>";
 echo "<li>".$fruits[4]."</li>";
 echo "</ul>";
+
+// print the array using for loop in index array
+echo "<br> print the array using for loop in index array <br>";
+for($i = 0; $i < count($fruits); $i++){
+    echo $fruits[$i]."<br>";
+}
+
+
+
+
+
 ?>
