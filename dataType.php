fc609bb6aa3f9791d5b877c1b28bf83018ddf719
MehediAlam49 <mehedialam806@gmail.com>
2025-02-03 22:36:08 +0600

    add integer and float data type code

--- dataType.php
+++ dataType.php
@@ -7,5 +7,26 @@ else:
     echo "Task is not completed";
 endif;
 
+echo "<br> <br>";
+
+// integer
+$age = 20;
+echo "The maximum value for integer is " . PHP_INT_MAX . "<br>";
+var_dump($age);
+
+echo "<br> <br>";
+
+// float
+$pi = 3.14;
+echo "The maximum value for float is " . PHP_FLOAT_MAX . "<br>";
+var_dump($pi);
+
+echo "<br> <br>";
+
+// Heredoc
+$firstName = "John";
+$lastName = "Doe";
+$fullName = $firstName . " " . $lastName;
+echo $fullName;
 
 ?>
