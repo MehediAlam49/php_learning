c369319092220aafb63a8108c4bf906cf7134d5b
MehediAlam49 <mehedialam806@gmail.com>
2025-02-10 20:50:11 +0600

    add a html form to insert data to database

---
+++ crud1/index.php
@@ -0,0 +1,23 @@
+<?php require_once('connection.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD1</title>
+</head>
+
+<body>
+    <div>
+        <form action="" method="post">
+            <input type="text" name="first_name" placeholder="First Name"> <br><br>
+            <input type="text" name="last_name" placeholder="Last Name"> <br><br>
+            <input type="number" name="age" placeholder="Age"> <br><br>
+            <input type="submit" value="Submit" name="submit_btn">
+        </form>
+    </div>
+</body>
+
+</html>
